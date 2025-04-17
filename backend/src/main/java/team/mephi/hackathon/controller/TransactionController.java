0bad10e6e1e4dbaf677597f1aff6e85c2e9053e9
Sofya Ivacheva <sofyaiv2405@gmail.com>
2025-04-17 18:41:07 +0700

    Sofya: test commit

--- backend/src/main/java/team/mephi/hackathon/controller/TransactionController.java
+++ backend/src/main/java/team/mephi/hackathon/controller/TransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
     private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
+
     @GetMapping("/transactions")
     public ResponseEntity<String> getTransactions() {
         logger.info("Test log record");
