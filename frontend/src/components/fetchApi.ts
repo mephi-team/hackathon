ac3825ecb5e5388c7e2eaa522683ba67e057ce9b
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-04-27 23:50:33 +0300

    use env

--- frontend/src/components/fetchApi.ts
+++ frontend/src/components/fetchApi.ts
@@ -2,9 +2,9 @@ import {Transaction} from "../interfaces/Transaction";
 
 export const fetchPdf = async () => {
   console.log('Вызов fetchPdf');
-  const response = await fetch("http://localhost:8000/api/reports/transactions/pdf", {
+  const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/reports/transactions/pdf`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+      "Authorization": `Bearer ${getAuth()}`
     }
   });
   return await response.blob();
@@ -12,9 +12,9 @@ export const fetchPdf = async () => {
 
 export const fetchExcel = async () => {
   console.log('Вызов fetchExcel');
-  const response = await fetch("http://localhost:8000/api/reports/transactions/excel", {
+  const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/reports/transactions/excel`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+      "Authorization": `Bearer ${getAuth()}`
     }
   });
   return await response.blob();
@@ -22,9 +22,9 @@ export const fetchExcel = async () => {
 
 export const fetchTransactions = async () => {
   console.log('Вызов fetchTransactions');
-  const response = await fetch("http://localhost:8000/api/transactions", {
+  const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/transactions`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+      "Authorization": `Bearer ${getAuth()}`
     }
   });
   return await response.json();
@@ -32,10 +32,10 @@ export const fetchTransactions = async () => {
 
 export const addTransaction = async (newTransaction: Transaction) => {
   console.log('Добавление транзакции:', newTransaction); // Отладочный лог
-  await fetch("http://localhost:8000/api/transactions", {
+  await fetch(`${import.meta.env.VITE_APP_API_URL}/api/transactions`, {
     method: "POST",
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+      "Authorization": `Bearer ${getAuth()}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify(newTransaction)
@@ -43,10 +43,10 @@ export const addTransaction = async (newTransaction: Transaction) => {
 };
 
 export const updateTransaction = async (updatedTransaction: Transaction) => {
-  await fetch(`http://localhost:8000/api/transactions/${updatedTransaction.id}`, {
+  await fetch(`${import.meta.env.VITE_APP_API_URL}/api/transactions/${updatedTransaction.id}`, {
     method: "PUT",
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+      "Authorization": `Bearer ${getAuth()}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify(updatedTransaction)
@@ -55,10 +55,10 @@ export const updateTransaction = async (updatedTransaction: Transaction) => {
 
 export const deleteTransaction = async (id: string) => {
   console.log('Удаление транзакции с ID:', id); // Отладочный лог
-  await fetch(`http://localhost:8000/api/transactions/${id}`, {
+  await fetch(`${import.meta.env.VITE_APP_API_URL}/api/transactions/${id}`, {
     method: "DELETE",
     headers: {
-      "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+      "Authorization": `Bearer ${getAuth()}`,
       "Content-Type": "application/json"
     }
   });
@@ -67,21 +67,26 @@ export const deleteTransaction = async (id: string) => {
 
 // Функция для имитации входа
 export const login = async ({ username, password }: { username: string; password: string }) => {
-  const response = await fetch("http://localhost:8080/realms/hackathon-realm/protocol/openid-connect/token", {
+  const response = await fetch(`${import.meta.env.VITE_APP_KEYCLOAK_URL}/realms/${import.meta.env.VITE_APP_KEYCLOAK_REALM}/protocol/openid-connect/token`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     },
-    body: `client_id=hackathon-frontend&grant_type=password&username=${username}&password=${password}`
+    body: `client_id=${import.meta.env.VITE_APP_KEYCLOAK_CLIENT_ID}&grant_type=password&username=${username}&password=${password}`
   })
   const json = await response.json();
   if (response.status === 200) {
     localStorage.setItem('isLoggedIn', 'true'); // Сохраняем статус авторизации
-    localStorage.setItem('access_token', json["access_token"]);
+    localStorage.setItem('accessToken', json["access_token"]);
     return true;
   }
   return false;
 };
+
+export const getAuth = () => {
+  return localStorage.getItem("accessToken");
+}
+
 
 // Проверка авторизации
 export const isAuthenticated = () => {
@@ -91,5 +96,5 @@ export const isAuthenticated = () => {
 // Выход из системы
 export const logout = async () => {
   localStorage.removeItem('isLoggedIn');
-  localStorage.removeItem('access_token');
+  localStorage.removeItem('accessToken');
 };
