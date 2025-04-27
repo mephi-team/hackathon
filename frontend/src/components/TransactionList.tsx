7c393dab9e42b08446e051d19bcca358537180f0
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-04-27 21:40:35 +0300

    fix

--- frontend/src/components/TransactionList.tsx
+++ frontend/src/components/TransactionList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, use} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Transaction } from '../interfaces/Transaction';
 import {
   fetchTransactions,
