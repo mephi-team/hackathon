36cd2ba46971f2f6c0b0e6007c855d14e37526b0
Mikhail Yudkin <kimopwnz@gmail.com>
2025-05-04 14:24:31 +0300

    Фикс

--- backend/src/main/java/team/mephi/hackathon/repository/TransactionRepository.java
+++ backend/src/main/java/team/mephi/hackathon/repository/TransactionRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team.mephi.hackathon.entity.Transaction;
+import team.mephi.hackathon.entity.TransactionType;
+import team.mephi.hackathon.entity.TransactionStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
