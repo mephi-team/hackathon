fc745208faadde7cfa82814abcb2b8ceaf6ca5d2
Mikhail Yudkin <kimopwnz@gmail.com>
2025-05-04 12:41:32 +0300

    Добавил к классу TransactionFilterDto аннотацию @NoArgsConstructor, обновил конструктор

--- backend/src/main/java/team/mephi/hackathon/dto/TransactionFilterDto.java
+++ backend/src/main/java/team/mephi/hackathon/dto/TransactionFilterDto.java
@@ -1,15 +1,20 @@
 package team.mephi.hackathon.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionFilterDto {
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-    private String transactionType;
-    private String status;
+    private String senderBank;
+    private String receiverBank;
+    private LocalDateTime dateFrom;
+    private LocalDateTime dateTo;
+    private BigDecimal amountMin;
+    private BigDecimal amountMax;
     private String category;
 }
