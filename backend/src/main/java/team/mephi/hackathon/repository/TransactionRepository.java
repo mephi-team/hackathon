19af37a1a751a1ad2389c97630c670244d7025b4
Mikhail Yudkin <kimopwnz@gmail.com>
2025-05-04 14:21:51 +0300

    Фикс

--- backend/src/main/java/team/mephi/hackathon/repository/TransactionRepository.java
+++ backend/src/main/java/team/mephi/hackathon/repository/TransactionRepository.java
@@ -16,13 +16,13 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
     @Query("""
-        SELECT t FROM Transaction t
-        WHERE (:dateFrom IS NULL OR CAST(:dateFrom AS timestamp) <= t.operationDate)
-            AND (:dateTo IS NULL OR CAST(:dateTo AS timestamp) >= t.operationDate)
-            AND (:transactionType IS NULL OR :transactionType = t.transactionType)
-            AND (:status IS NULL OR :status = t.status)
-            AND (:category IS NULL OR :category = t.category)
-            AND t.deleted = false
+    SELECT t FROM Transaction t
+    WHERE (:dateFrom IS NULL OR CAST(:dateFrom AS timestamp) <= t.operationDate)
+      AND (:dateTo IS NULL OR CAST(:dateTo AS timestamp) >= t.operationDate)
+      AND (:transactionType IS NULL OR :transactionType = t.transactionType)
+      AND (:status IS NULL OR :status = t.status)
+      AND (:category IS NULL OR :category = t.category)
+      AND t.deleted = false
     """)
     List<Transaction> findAllByFilter(
             @Param("dateFrom") LocalDateTime dateFrom,
