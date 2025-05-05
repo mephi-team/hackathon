0dc14dfc48075e792ca0b4978335d1a447091bea
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-05-05 23:58:24 +0300

    fix for tests

--- backend/src/test/java/team/mephi/hackathon/service/TransactionServiceImplTest.java
+++ backend/src/test/java/team/mephi/hackathon/service/TransactionServiceImplTest.java
@@ -28,7 +28,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
